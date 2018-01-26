10b622b4ac7c3a425f764607f72c9cd14a7ca2ff
开心小乔 <3050315638@qq.com>
2018-01-26 10:40:24 +0800

    refactor

--- src/chinesecard/Cards.java
+++ src/chinesecard/Cards.java
@@ -1,5 +1,24 @@
 package chinesecard;
 
+import java.util.ArrayList;
+
 public class Cards {
-	
+	 static String[] NUMBER = new String[]{"1","2","3","4","5","6","7","8","9"};
+	 static String[] COLOR = new String[]{"tiao","bing","wang"};
+	 public  ArrayList<Card> create(){
+		    ArrayList<Card> cards= new ArrayList<Card>();
+			addBaseCards(cards);
+			return cards;
+	 }
+
+
+	private void addBaseCards(ArrayList<Card> cards) {
+
+		for( int i=0;i<NUMBER.length ;i++){
+			 for(int j=0;j<COLOR.length;j++){
+				 Card card = new Card(NUMBER[i],COLOR[j]);
+				 cards.add(card);
+			 }
+		 }		
+	}
 }
--- src/chinesecard/Game.java
+++ src/chinesecard/Game.java
@@ -3,21 +3,10 @@ package chinesecard;
 import java.util.ArrayList;
 public class Game {
 	public static void main(String[] args){
-		 final ArrayList<String> COLOR = new ArrayList<String>();
-		 String[] NUMBER = new String[]{"1","2","3","4","5","6","7","8","9"};
-		 COLOR.add("条");
-		 COLOR.add("wan");
-		 COLOR.add("bing");
-		 ArrayList<Card> cards= new ArrayList<Card>();
-		 
-		 for( int i=0;i<NUMBER.length ;i++){
-			 for(int j=0;j<COLOR.size();j++){
-				 Card card = new Card(NUMBER[i],COLOR.get(j));
-				 cards.add(card);
-			 }
-		 }
-		 for(int i=0;i<cards.size();i++){
-			 System.out.println(cards.get(i));
+        Cards mycard = new Cards();
+		ArrayList<Card> mycards=mycard.create();
+		 for(int i=0;i<mycards.size();i++){
+			 System.out.println(mycards.get(i));
 		 }
 	}
 }
